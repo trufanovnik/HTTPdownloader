0a08376c7ef6922a8401babd15171ad2aaaecbce
unknown <trufanovwork@gmail.com>
2025-02-10 15:20:14 +0300

    feature to get downloadable links from default branch on GitHub rep

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        GetDownloadLinkFromGithub.getAndSaveDownloadLinks();
     }
 }
